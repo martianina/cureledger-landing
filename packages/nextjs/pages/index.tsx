a733a9bebf20153c40b43022f13632dd43b8f4b9
martianina <56848071+martianina@users.noreply.github.com>
2024-10-02 14:34:41 -0400

    Update index.tsx for DeSciLife

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -14,13 +14,13 @@ const Home: NextPage = () => {
       >
         <div className="flex items-center gap-2">
           <div className="flex relative w-10 h-10">
-            <Image alt="CureLedger Logo" className="cursor-pointer" fill src="/favicon.png" />
+            <Image alt="DeSciLife Logo" className="cursor-pointer" fill src="/favicon.png" />
           </div>
-          <p className="text-3xl m-0 font-medium mt-1">CureLedger</p>
+          <p className="text-3xl m-0 font-medium mt-1">DeSciLife</p>
         </div>
         <div className="flex flex-col justify-center items-center gap-8 mb-14 px-4">
           <h1 className="text-center text-3xl lg:text-5xl max-w-md lg:max-w-2xl px-3 m-0">
-            Improving Global Health with Data Innovation
+            The Verifiable Health Data Marketplace
           </h1>
           <h3 className="text-center text-3xl lg:text-2xl max-w-md lg:max-w-2xl px-3 m-0">
             Proven blockchain-based health data solutions
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
           <h2 className="text-center text-3xl lg:text-4xl max-w-md lg:max-w-2xl px-3 m-0">1,000,000+ Lives Impacted</h2>
           <div className="flex flex-wrap gap-4 items-center justify-center">
             <a
-              href="https://docsend.com/view/6d98ecgkr8vkxfb5"
+              href="https://docsend.com/view/thfa4mppugrurxmi"
               className="btn btn-outline btn-sm px-5 h-10 bg-base-100 normal-case font-normal text-lg"
             >
               White Paper
@@ -48,15 +48,15 @@ const Home: NextPage = () => {
         <div className="container max-w-[90%] lg:max-w-7xl m-auto py-16 lg:py-20 lg:pl-12 lg:pr-6 flex flex-col-reverse lg:flex-row items-center gap-5 lg:gap-0">
           <div className="space-y-6">
             <div className="flex items-center justify-center lg:flex-col lg:items-start lg:justify-start gap-2 pt-4 lg:pt-0">
-              <Image src="/favicon.png" alt="cureledger logo" height={30} width={30} />
+              <Image src="/favicon.png" alt="DeSciLife logo" height={30} width={30} />
               <p className="text-center lg:text-left text-xl m-0 font-light">DEDICATED TO GLOBAL HEALTH</p>
             </div>
 
             <h2 className="text-2xl lg:text-4xl lg:w-4/5 text-center lg:text-left font-medium">
-              CureLedger is blockchain technology used for its highest purpose: improving human health
+              DeSciLife is blockchain technology used for its highest purpose: improving human health
             </h2>
             <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
-              With Cureledger, individual data sovereignty underpins health care data reuse and monetization.
+              With DeSciLife, individual data sovereignty underpins health care data reuse and monetization.
             </p>
             <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
               Why is this revolutionary?
@@ -68,7 +68,7 @@ const Home: NextPage = () => {
               Until decentralized blockchain technology, there was no viable way to harness that data or its value.
             </p>
             <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
-              CureLedger is the place where this valuable data is delivered in reusable, merchantable packages to a
+              DeSciLife is the place where this valuable data is delivered in reusable, merchantable packages to a
               vibrant user exchange of contributors and consumers verifiable health data.
             </p>
           </div>
@@ -83,12 +83,12 @@ const Home: NextPage = () => {
         <div className="container max-w-[90%] lg:max-w-7xl m-auto py-16 lg:py-20 lg:px-12 flex flex-col lg:flex-row justify-between items-center gap-5 lg:gap-0">
           <div className="flex flex-col items-center">
             <div className="max-w-[400px] lg:max-w-none">
-              <Image src="/assets/compliance-flow.png" alt="Cureledger Compliance" width={2500} height={2500} />
+              <Image src="/assets/compliance-flow.png" alt="DeSciLife Compliance" width={2500} height={2500} />
             </div>
           </div>
           <div className="space-y-6 flex flex-col items-center lg:items-end">
             <div className="flex items-center justify-center lg:flex-col lg:items-start lg:justify-start gap-2 lg:w-3/4 pt-4 lg:pt-0">
-              <Image src="/favicon.png" alt="cureledger logo" height={20} width={20} />
+              <Image src="/favicon.png" alt="descilife logo" height={20} width={20} />
               <p className="text-center lg:text-left text-xl m-0 font-light">COMPLIANCE CORE</p>
             </div>
             <div className="lg:w-3/4 space-y-5">
@@ -96,7 +96,7 @@ const Home: NextPage = () => {
                 Proven Compliance Primitives Enhance Data Reusability, Merchantability & Liquidity
               </h2>
               <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
-                Exclusive incentives & early access to CureLedger for DeSci projects
+                Exclusive incentives & early access to DeSciLife for DeSci projects
               </p>
               <p className="m-auto text-center lg:text-left lg:mx-0 max-w-[300px] lg:max-w-none lg:w-3/4">
                 Our goal is to jump-start YOUR healthcare technology success
@@ -112,8 +112,8 @@ const Home: NextPage = () => {
         <div className="container max-w-[90%] lg:max-w-7xl m-auto py-16 lg:py-20 lg:pl-12 lg:pr-6 flex flex-col-reverse lg:flex-row items-center gap-5 lg:gap-0">
           <div className="space-y-6">
             <div className="flex items-center justify-center lg:flex-col lg:items-start lg:justify-start gap-2 pt-4 lg:pt-0">
-              <Image src="/favicon.png" alt="cureledger logo" height={30} width={30} />
-              <p className="text-center lg:text-left text-xl m-0 font-light">CURELEDGER MARKETPLACE</p>
+              <Image src="/favicon.png" alt="descilife logo" height={30} width={30} />
+              <p className="text-center lg:text-left text-xl m-0 font-light">DESCILIFE MARKETPLACE</p>
             </div>
 
             <h2 className="text-2xl lg:text-4xl lg:w-4/5 text-center lg:text-left font-medium">
@@ -141,15 +141,15 @@ const Home: NextPage = () => {
       {/* Buidl in Community */}
       <div className="bg-[url(/assets/network.png)] h-[576px] bg-cover bg-center flex items-end justify-center">
         <div className="flex flex-col justify-center items-center bg-base-200 w-4/5 max-w-3xl lg:w-5/12 rounded-3xl shadow-lg shadow-primary p-10 gap-4 -mb-12">
-          <Image src="/favicon.png" alt="cureledger logo" height={30} width={30} />
+          <Image src="/favicon.png" alt="DeSciLife logo" height={30} width={30} />
           <p className="text-center text-3xl font-medium m-0">DSCI Token</p>
           <p className="text-center m-0">Innovative Tokenomics</p>
-          <p className="text-center m-0">Provides access to CureLedger Products</p>
-          <p className="text-center m-0">Powers the CureLedger Marketplace</p>
+          <p className="text-center m-0">Provides access to DeSciLife Products</p>
+          <p className="text-center m-0">Powers the DeSciLife Marketplace</p>
           <p className="text-center m-0">Utilizing MiCA for compliance</p>
 
           <a
-            href="https://docsend.com/view/6d98ecgkr8vkxfb5"
+            href="https://docsend.com/view/thfa4mppugrurxmi"
             className="btn btn-outline btn-sm px-5 h-10 bg-base-100 normal-case font-normal text-lg"
           >
             Read White Paper
@@ -170,12 +170,12 @@ const Home: NextPage = () => {
 
           <div className="space-y-6 flex flex-col items-center lg:pl-16">
             <div className="flex items-center justify-center lg:flex-col lg:items-start lg:justify-start gap-2 lg:w-3/4 pt-4 lg:pt-0">
-              <Image src="/favicon.png" alt="cureledger logo" height={20} width={20} />
+              <Image src="/favicon.png" alt="DeSciLife logo" height={20} width={20} />
               <p className="text-center lg:text-left text-xl m-0 font-light">VALIDATORS</p>
             </div>
             <div className="lg:w-3/4 space-y-6">
               <h2 className="text-2xl lg:text-4xl text-center lg:text-left font-medium">
-                CureLedger’s validators underpin the most valuable and impactful human data set in the world.
+                DeSciLife’s validators underpin the most valuable and impactful human data set in the world.
               </h2>
               {/* Updated whitepaper link */}
               <div className="flex flex-wrap gap-4 items-center justify-center"></div>
