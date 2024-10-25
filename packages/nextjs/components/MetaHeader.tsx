6144253d35aeab2ebbb281ede091018dc09fb792
Nina Kilbride <nina@marsmetro.com>
2024-10-25 10:52:41 -0400

    typp

--- packages/nextjs/components/MetaHeader.tsx
+++ packages/nextjs/components/MetaHeader.tsx
@@ -14,7 +14,7 @@ type MetaHeaderProps = {
 const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/` : "/";
 
 export const MetaHeader = ({
-  title = "CureLEdger",
+  title = "CureLedger",
   description = "Verifiable Health Data Marketplace",
   image = "/assets/icon-violet.png",
   twitterCard = "summary_large_image",
