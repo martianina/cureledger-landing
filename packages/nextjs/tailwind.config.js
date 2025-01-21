1eab39236a1520c3879404845519a786703f8962
martianina <56848071+martianina@users.noreply.github.com>
2025-01-21 10:27:51 -0500

    Update tailwind.config.js
    change color description

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -8,22 +8,22 @@ module.exports = {
     themes: [
       {
         customDarkTheme: {
-          primary: "#56c5fb", // Bright periwinkle/violet
-          "primary-content": "#FFFFFF",
-          secondary: "#000000", // Cool black
-          "secondary-content": "#fd9905", // Light violet
-          accent: "#56c5fb", // Bright periwinkle/violet
-          "accent-content": "#FFFFFF",
-          neutral: "#ededed", // Very light violet
-          "neutral-content": "#000000", // Cool black
-          "base-100": "#000000", // Cool black base
-          "base-200": "#000000", // Slightly lighter black
-          "base-300": "#000000", // Even lighter black
-          "base-content": "#FFFFFF", // White text content
-          info: "#ffa810", // Light violet
-          success: "#2baceb", // Bright periwinkle/violet
-          warning: "#ffa810", // Very light violet
-          error: "#ffa810", // Light violet
+    primary: "#56c5fb", // Bright sky blue
+    "primary-content": "#FFFFFF", // White text content
+    secondary: "#000000", // Black
+    "secondary-content": "#fd9905", // Vibrant orange
+    accent: "#56c5fb", // Bright sky blue
+    "accent-content": "#FFFFFF", // White text content
+    neutral: "#ededed", // Light gray
+    "neutral-content": "#000000", // Black text content
+    "base-100": "#000000", // Black base
+    "base-200": "#000000", // Black (no variation in shade)
+    "base-300": "#000000", // Black (no variation in shade)
+    "base-content": "#FFFFFF", // White text content
+    info: "#ffa810", // Vibrant orange
+    success: "#2baceb", // Bright cyan-blue
+    warning: "#ffa810", // Vibrant orange
+    error: "#ffa810", // Vibrant orange
 
           ".tooltip": {
             "--tooltip-tail": "6px",
